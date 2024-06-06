e9ca2d3a59598d38f7ff413db9ce0539bc024751
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2024-06-05 21:28:01 -0300

    feat: add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config(); // Carregar variáveis de ambiente do arquivo .env
+
 const express = require('express');
 const path = require('path');
 const si = require('systeminformation');
 const Docker = require('dockerode');
+const { exec } = require('child_process');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Utiliza a variável de ambiente PORT ou o valor padrão 5000
+const folderPath = process.env.FOLDER_PATH || '/home/jieff/dev/ksys/scan'; // Utiliza a variável de ambiente FOLDER_PATH ou o valor padrão
+
 const docker = new Docker();
 
 app.use(express.static(path.join(__dirname, '')));
@@ -20,7 +25,7 @@ app.get('/info/api', async (req, res) => {
       getDockerStats(),
     ]);
 
-    const folderDiskUsage = await getFolderDiskUsage('/home/jieff/dev/ksys/scan');
+    const folderDiskUsage = await getFolderDiskUsage(folderPath);
 
     res.json({
       cpu,
@@ -29,7 +34,7 @@ app.get('/info/api', async (req, res) => {
       currentLoad,
       disk,
       containers: containerStats,
-      folderDiskUsage
+      folderDiskUsage,
     });
   } catch (error) {
     res.status(500).send(error.toString());
@@ -38,14 +43,10 @@ app.get('/info/api', async (req, res) => {
 
 app.get('/disk-usage', async (req, res) => {
   try {
-    let folderPath = req.query.folderPath;
+    let requestedFolderPath = req.query.folderPath || folderPath;
 
-    if (!folderPath) {
-      folderPath = '/home/jieff/dev/ksys/scan';
-    }
-
-    const diskUsage = await getFolderDiskUsage(folderPath);
-    res.json({ folderPath, diskUsage });
+    const diskUsage = await getFolderDiskUsage(requestedFolderPath);
+    res.json({ folderPath: requestedFolderPath, diskUsage });
   } catch (error) {
     console.error(`Erro ao obter o uso de disco da pasta: ${error.message}`);
     res.status(500).send(error.toString());
@@ -63,19 +64,21 @@ app.get('/', (req, res) => {
 async function getDockerStats() {
   try {
     const containers = await docker.listContainers();
-    const stats = await Promise.all(containers.map(async (containerInfo) => {
-      const container = docker.getContainer(containerInfo.Id);
-      const data = await container.stats({ stream: false });
-      return {
-        name: containerInfo.Names[0],
-        cpu: (data.cpu_stats.cpu_usage.total_usage / data.cpu_stats.system_cpu_usage) * 100,
-        memory: {
-          total: data.memory_stats.limit,
-          usage: data.memory_stats.usage
-        },
-        diskUsage: await getContainerDiskUsage(containerInfo.Id)
-      };
-    }));
+    const stats = await Promise.all(
+      containers.map(async (containerInfo) => {
+        const container = docker.getContainer(containerInfo.Id);
+        const data = await container.stats({ stream: false });
+        return {
+          name: containerInfo.Names[0],
+          cpu: (data.cpu_stats.cpu_usage.total_usage / data.cpu_stats.system_cpu_usage) * 100,
+          memory: {
+            total: data.memory_stats.limit,
+            usage: data.memory_stats.usage,
+          },
+          diskUsage: await getContainerDiskUsage(containerInfo.Id),
+        };
+      })
+    );
     return stats;
   } catch (error) {
     console.error('Erro ao obter informações dos containers:', error);
@@ -88,7 +91,6 @@ async function getContainerDiskUsage(containerId) {
 }
 
 async function getFolderDiskUsage(folderPath) {
-  const { exec } = require('child_process');
   return new Promise((resolve, reject) => {
     exec(`du -sm ${folderPath}`, (error, stdout) => {
       if (error) {
